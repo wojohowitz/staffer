34ab0ee8b9705f7f6ea14a354174f8f1d840191d
wojohowitz <wojtek.augustynski@comcast.net>
2016-08-22 23:56:42 -0700

    sequelize initialized with sqlite dev; initial migration to test connection

---
+++ db/sequelize/migrations/20160823063902-createUsers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      }, 
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: function (queryInterface, Sequelize) {
+      return queryInterface.dropTable('users');
+  }
+};
